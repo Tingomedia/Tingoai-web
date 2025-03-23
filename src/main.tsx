e122a1cc8eec43df81bd0c3a3b4194997f8f41b7
Blard <team@tingomedia.ai>
2025-03-24 00:06:14 +0100

    Songs upload interface

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import "./index.css";
 import { AppProvider } from "./contexts/AppContext.tsx";
 import { BrowserRouter } from "react-router-dom";
 import CustomLoader from "./utils/libs/CustomLoader.tsx";
+import ErrorHandler from "./hooks/ErrorHandler.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Suspense fallback={<CustomLoader />}>
       <BrowserRouter>
         <AppProvider>
+          <ErrorHandler>
             <App />
+          </ErrorHandler>
         </AppProvider>
       </BrowserRouter>
     </Suspense>
