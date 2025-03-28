e9b4d468fd6acd7ee150b4ba3fdc0fe4fa45cec4
Ravi80335 <Ravisankar.Bommu@gmail.com>
2025-03-28 23:13:03 +0530

    Graphic change in GPT Landing page

--- src/components/gpt/v2/GptLandingPage.tsx
+++ src/components/gpt/v2/GptLandingPage.tsx
@@ -4,39 +4,44 @@ import { Link } from "react-router-dom";
 
 const GptLandingPage = () => {
     return (
-        <div className="w-full h-svh font-Cera tracking-wider relative flex flex-col items-center justify-center text-white bg-purple/50">
-            {/* Birds in the background */}
-            <AnimGraphic />
+      <div className="w-full h-svh font-Cera tracking-wider relative flex flex-col items-center justify-center text-white bg-purple/50">
+        {/* Birds in the background */}
+        <AnimGraphic orangeGraphic />
 
-            {/* Hero Text & CTA Button */}
-            <div className="absolute text-center top-[20%]">
-                <div className="w-full flex justify-center items-center">
-                    <img src="/icons/gpt-logo.svg" alt="" className="h-64" />
-                </div>
+        {/* Hero Text & CTA Button */}
+        <div className="absolute text-center top-[20%]">
+          <div className="w-full flex justify-center items-center">
+            <img src="/icons/gpt-logo.svg" alt="" className="h-64" />
+          </div>
 
-                <div className="w-full flex justify-center gap-8 mt-6">
-                    <Link to="/tingogpt" className="px-6 py-3 border-2 bg-transparent text-white font-semibold rounded-full shadow-lg hover:scale-105 transition">
-                        TingoGPT
-                    </Link>
-                    <Link to="#" className="px-6 py-3 border-2 bg-transparent text-white font-semibold rounded-full shadow-lg hover:scale-105 transition">
-                        TeenGPT
-                    </Link>
-                </div>
-            </div>
-
-            {/* Footer */}
-            <footer className="absolute bottom-8 text-center  opacity-70">
-
-                <div className="">
-                    <Link to="/" className="w-full flex justify-center items-center">
-                        <img src="/icons/logo-footer.svg" alt="" className=" w-32" />
-                    </Link>
-                    <div className="font-Cera text-white">
-                        Terms of use | Privacy policy
-                    </div>
-                </div>
-            </footer>
+          <div className="w-full flex justify-center gap-8 mt-6">
+            <Link
+              to="/tingogpt"
+              className="px-6 py-3 border-2 bg-transparent text-white font-semibold rounded-full shadow-lg hover:scale-105 transition"
+            >
+              TingoGPT
+            </Link>
+            <Link
+              to="#"
+              className="px-6 py-3 border-2 bg-transparent text-white font-semibold rounded-full shadow-lg hover:scale-105 transition"
+            >
+              TeenGPT
+            </Link>
+          </div>
         </div>
+
+        {/* Footer */}
+        <footer className="absolute bottom-8 text-center  opacity-70">
+          <div className="">
+            <Link to="/" className="w-full flex justify-center items-center">
+              <img src="/icons/logo-footer.svg" alt="" className=" w-32" />
+            </Link>
+            <div className="font-Cera text-white">
+              Terms of use | Privacy policy
+            </div>
+          </div>
+        </footer>
+      </div>
     );
 };
 
--- src/pages/auth/v2/AnimGraphic.tsx
+++ src/pages/auth/v2/AnimGraphic.tsx
@@ -1,13 +1,17 @@
 import useWindowWidth from "../../../hooks/useWindowWidth";
 import AnimatedBirdsCSS from "./AnimatedBirdsCSS";
 
-export default function AnimGraphic() {
+export default function AnimGraphic({ orangeGraphic = false }) {
   const { width } = useWindowWidth();
   return (
     <div className="absolute w-svw h-svh overflow-clip bg-gray-400 -z-10">
       <AnimatedBirdsCSS speed={20} />
       <img
-        src="/graphics/TingoStripes-01.svg"
+        src={
+          orangeGraphic
+            ? "/graphics/TingoStripes-02.svg"
+            : "/graphics/TingoStripes-01.svg"
+        }
         className="absolute -bottom-[64px] -left-[84px] max-w-full h-auto"
         width={width * 0.442}
         height={((width * 0.442) / 1.476).toFixed()}
