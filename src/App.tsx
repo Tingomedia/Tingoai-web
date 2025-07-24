78fd0e15a6ae8c6c1fca19716c87b53c4153466e
Ravi80335 <Ravisankar.Bommu@gmail.com>
2025-07-24 13:46:13 +0530

    gpt page as root

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import { lazy } from "react";
 import { ScrollToTop } from "./utils/helpers/SmoothScroll";
-import NewGridHome from "./components/landing/new/NewGridHome";
 import ProtectedRoute from "./components/common/ProtectedRoute";
 import GptHomeV2 from "./components/gpt/v2/GptHome";
-import GptTeenWaitList from "./components/gpt/v2/GptTeenWaitlist";
-import GptLandingPage from "./components/gpt/v2/GptLandingPage"; // GPTLandingPage
-const Home = lazy(() => import("./pages/landingPage/Home"));
 const Page404 = lazy(() => import("./pages/landingPage/Page404"));
-const Product = lazy(() => import("./pages/landingPage/old_website/Products"));
-const Contact = lazy(() => import("./pages/landingPage/old_website/Contact"));
-const About = lazy(() => import("./pages/landingPage/old_website/About"));
-const TingoaiLayout = lazy(() => import("./components/tingoai/TingoaiLayout"));
 const Login = lazy(() => import("./pages/auth/v2/Login"));
 const GptPlusHome = lazy(() => import("./components/gpt/GptPlusHome"));
-const OtpCode = lazy(() => import("./pages/auth/OtpCode"));
-const OtpMail = lazy(() => import("./pages/auth/OtpMail"));
-const ResetPassword = lazy(() => import("./pages/auth/ResetPassword"));
-const TingoaiProducts = lazy(
-  () => import("./components/tingoai/TingoaiProducts")
-);
-const RadioLayout = lazy(() => import("./layouts/radio/RadioLayout"));
-const RadioHome = lazy(() => import("./pages/old_radio/radio/RadioHome"));
-const Playlist = lazy(() => import("./pages/old_radio/radio/Playlist"));
-const NewsWeather = lazy(() => import("./pages/old_radio/radio/News&weather"));
-const Reachus = lazy(() => import("./pages/old_radio/radio/Reachus"));
 const RadioSongsUpload = lazy(
   () => import("./layouts/radio/general/RadioSongsUpload")
 );
@@ -35,47 +16,14 @@ function App() {
     <>
       <ScrollToTop />
       <Routes>
-        {/* General Routes */}
-        <Route path="/" element={<Home />} />
-
-        <Route path="/tingoai-products" element={<NewGridHome />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/products" element={<Product />} />
-        <Route path="/otp-code" element={<OtpCode />} />
-        <Route path="/otp-mail" element={<OtpMail />} />
-        <Route path="/reset-password" element={<ResetPassword />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signin" element={<Navigate to="/login" replace />} />
-        <Route path="/signup" element={<Navigate to="/login" replace />} />
-        <Route path="/upgrade" element={<GptPlusHome />} />
-        <Route path="/gpt-home" element={<GptLandingPage />} />
-        <Route path="/gpt-teen-waitlist" element={<GptTeenWaitList />} />
-        <Route path="/gpt-mobile-waitlist" element={<GptTeenWaitList App />} />
 
-        {/*=========== TingoGPT Routes ================*/}
-        <Route
-          path="/tingogpt"
-          element={<ProtectedRoute element={<GptHomeV2 />} />}
-        >
+        <Route path="/" element={<ProtectedRoute element={<GptHomeV2 />} />}>
           <Route path="" element={<GptHomeV2 />} />
           <Route path="plus" element={<GptPlusHome />} />
         </Route>
 
-        {/*============== TingoAI Routes =============*/}
-        <Route path="/tingoai" element={<TingoaiLayout />}>
-          <Route path="" element={<TingoaiProducts />} />
-        </Route>
-
-        {/*============== Tingo Radio =============*/}
-        <Route path="/radio" element={<RadioLayout />}>
-          <Route path="" element={<RadioHome />} />
-          <Route path="playlists" element={<Playlist />} />
-          <Route path="news" element={<NewsWeather />} />
-          <Route path="reachus" element={<Reachus />} />
-        </Route>
-
-        <Route path="/test" element={<GptHomeV2 />} />
+        {/* <Route path="/test" element={<GptHomeV2 />} /> */}
         <Route path="/radio-upload" element={<RadioSongsUpload />} />
         {/* 404 Page */}
         <Route path="*" element={<Page404 />} />
